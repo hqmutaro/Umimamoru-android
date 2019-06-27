23dc4f4e5c412f3dd7a0471beca0dfe1f17e7d3e
github nitf <2tf2003@gmail.com>
2019-06-27 18:51:51 +0900

    add: メイン画面実装

---
+++ app/src/main/java/com/github/nitf/umimamoru/ui/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.github.nitf.umimamoru.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.nitf.umimamoru.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.selectButton);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SelectRegionActivity.class);
+        startActivityForResult(intent, 0);
+    }
+}
