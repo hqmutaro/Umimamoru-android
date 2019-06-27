0d74b5c0909e8fc925e60294789f9b4072d6d452
github nitf <2tf2003@gmail.com>
2019-06-27 18:51:18 +0900

    add: InfoDisplayActivity実装

---
+++ app/src/main/java/com/github/nitf/umimamoru/ui/activity/InfoDisplayActivity.java
@@ -0,0 +1,22 @@
+package com.github.nitf.umimamoru.ui.activity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.github.nitf.umimamoru.R;
+
+public class InfoDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_display);
+    }
+}
