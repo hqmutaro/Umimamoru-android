10dfd45105c3c8179a392a0cbaad77af99e9bc5a
github nitf <2tf2003@gmail.com>
2019-06-29 00:13:47 +0900

    feat: 離岸流が発生しているか否かで表示する警告を変更させる
    まだ離岸流の発生判定を実装していないので、isOccurRipCurrentにはtrue、発生していると仮定した場合のコードを書いた。

---
+++ app/src/main/java/com/github/nitf/umimamoru/ui/fragment/WarningDisplayFragment.java
@@ -0,0 +1,29 @@
+package com.github.nitf.umimamoru.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.nitf.umimamoru.R;
+
+public class WarningDisplayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        boolean isOccurRipCurrent = true;
+        int resource;
+        resource = (isOccurRipCurrent) ? R.layout.fragment_warning_display : R.layout.fragment_all_clear_display;
+        return inflater.inflate(resource, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
