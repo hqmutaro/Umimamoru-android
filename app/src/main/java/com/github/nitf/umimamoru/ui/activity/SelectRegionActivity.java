26a758f3087d0d735039173652bf9269947f058e
github nitf <2tf2003@gmail.com>
2019-07-02 00:18:55 +0900

    feat: 地域の検索機能を実装

--- app/src/main/java/com/github/nitf/umimamoru/ui/activity/SelectRegionActivity.java
+++ app/src/main/java/com/github/nitf/umimamoru/ui/activity/SelectRegionActivity.java
@@ -4,36 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.github.nitf.umimamoru.ui.utils.ActivityRepsitory;
-import com.github.nitf.umimamoru.ui.utils.CustomAdapter;
 import com.github.nitf.umimamoru.R;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SelectRegionActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener {
 
     private ListView listView;
 
-    private static final String[] regions = {
-            "foo",
-            "bar",
-            "foobar",
-            "hoge",
-            "hogehoge"
-    };
+    private static String[] regions;
 
-    static final String ACTIVITY_NAME = "SelectRegion";
+    static class ViewHolder {
+        Button selectButton;
+    }
 
-    public SelectRegionActivity(){
+    public SelectRegionActivity() {
+        SelectRegionActivity.regions = new String[]{
+                "foo",
+                "bar",
+                "foobar",
+                "hoge",
+                "hogehoge"
+        }; // 鯖から取得するらしい(草)
     }
 
     @Override
@@ -42,15 +46,17 @@ public class SelectRegionActivity extends AppCompatActivity implements AdapterVi
         setContentView(R.layout.activity_select_region);
         ActivityRepsitory.setPreviousActivity("Main");
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, regions);
         listView = (ListView) findViewById(R.id.listView);
-        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), R.layout.row_item, regions);
-        listView.setAdapter(adapter);
+        listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(this);
         listView.setTextFilterEnabled(true);
 
         SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setIconifiedByDefault(false);
         searchView.setOnQueryTextListener(this);
         searchView.setSubmitButtonEnabled(false);
+        searchView.setQueryHint("地域名");
     }
 
     @Override
@@ -62,18 +68,17 @@ public class SelectRegionActivity extends AppCompatActivity implements AdapterVi
     }
 
     @Override
-    public boolean onQueryTextChange(String text) {
-        if (TextUtils.isEmpty(text)) {
+    public boolean onQueryTextChange(String queryText) {
+        if (TextUtils.isEmpty(queryText)) {
             listView.clearTextFilter();
+        } else {
+            listView.setFilterText(queryText.toString());
         }
-        else {
-            listView.setFilterText(text.toString());
-        }
-        return false;
+        return true;
     }
 
     @Override
-    public boolean onQueryTextSubmit(String query) {
+    public boolean onQueryTextSubmit(String s) {
         return false;
     }
 }
