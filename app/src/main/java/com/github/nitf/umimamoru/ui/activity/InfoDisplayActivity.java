f1ac7c485f9f5f9cf896d16358ebddfb5c428807
github nitf <2tf2003@gmail.com>
2019-07-02 00:18:24 +0900

    debug: デバッグ用にテキストを設定

--- app/src/main/java/com/github/nitf/umimamoru/ui/activity/InfoDisplayActivity.java
+++ app/src/main/java/com/github/nitf/umimamoru/ui/activity/InfoDisplayActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.github.nitf.umimamoru.R;
 
@@ -18,5 +19,11 @@ public class InfoDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_display);
+        // TODO: StateInfoからデータを取得して情報を表示させる
+        // 以下、デバッグ用の固定情報
+        String region = "沖縄県名護市辺野古豊原\n" + "シーグラスビーチ" + "の波の様子";
+
+        TextView regionView = (TextView) findViewById(R.id.regionView);
+        regionView.setText(region);
     }
 }
